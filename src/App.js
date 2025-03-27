bb2f46a0b34113f4ba03b1dbdac6ab252c8f11e3
TedBarbier <ted.barbier@proton.me>
2025-03-27 17:10:27 +0100

    fr version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import './index.css';
 
-function PracticeExamWebsite() {
+function App() {
   return (
     <div className="exam-container">
       <h1>Partiel de Droit Informatique - Examen d'Entraînement</h1>
-
       <section className="instructions">
         <h2>Instructions:</h2>
         <ol>
